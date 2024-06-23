d3b215945cc51ce5b56b489ed6dd3cb1cd81eeca
facundo1000 <martinez.facundo85@gmail.com>
2024-06-23 11:47:36 -0300

    the dog eat the code

--- README.md
+++ README.md
@@ -2,11 +2,9 @@
 
 ## Utilización
 
-Ingresar al directorio `analizador-lexico` y dentro del mismo ingresar a la carpeta 
-`target` dentro encontrará un archivo `analizador-lexico.java`.
+1. Descomprimir el archivo `analizador-lexico.zip`.
+2. Ingresar al directorio `analizador-lexico`.
+3. Ejecutar el comando `mvn clean package` o `mvn clean install` para compilar el proyecto (se debe tener maven instalado).
+4. Luego de compilar el proyecto, ingresar al directorio `target` y ejecutar el comando `java -jar analizador-lexico.jar`.
 
-Para ejecutar el programa, se debe ingresar el siguiente comando desde la consola (estando dentro de la carpeta `target`):
-
-``java -jar analizador-lexico.jar``
-
-El programa finaliza cuando se ingresa o lee exactamente la palabra `fin`.
+**El programa finaliza cuando se ingresa o lee exactamente la palabra `fin`.**
--- src/main/java/dev/fmartinez/Main.java
+++ src/main/java/dev/fmartinez/Main.java
@@ -1,29 +1,54 @@
 package dev.fmartinez;
 
-import java.util.List;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("-------Inicio del programa-------");
-        Scanner scanner = new Scanner(System.in);
+//        Scanner scanner = new Scanner(System.in);
         StringBuilder code = new StringBuilder();
+//
+//        while (scanner.hasNextLine()) {
+//            code.append("\n").append(scanner.nextLine());
+//
+//            if (code.toString().contains("fin")) {
+//                break;
+//            }
+//        }
+//        scanner.close();
+//
+//
+//        Lexer lexer = new Lexer(code.toString());
+//        List<Token> tokens = lexer.tokenize();
+//        for (Token token : tokens) {
+//            System.out.println(token);
+//        }
 
-        while (scanner.hasNextLine()) {
-            code.append("\n").append(scanner.nextLine());
+        if (args.length != 1) {
+            System.err.println("Usage: java -jar lexical-analyzer.jar <input-file>");
+            System.exit(1);
+        }
 
-            if (code.toString().contains("fin")) {
-                break;
+        String filename = args[0];
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+
+            while (reader.readLine() != null) {
+                code.append(reader.readLine());
             }
-        }
-        scanner.close();
+            Lexer lexer = new Lexer(code.toString());
 
-
-        Lexer lexer = new Lexer(code.toString());
-        List<Token> tokens = lexer.tokenize();
-        for (Token token : tokens) {
-            System.out.println(token);
+            for (Token token : lexer.tokenize()) {
+                System.out.println(token);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("File read successfully");
         }
+
         System.out.println("-------Fin del programa-------");
     }
 }
